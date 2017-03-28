6b5975085beacb9c480f5f85db8edf8c8f4d5c69
Seth Whiting <sethcwhiting@gmail.com>
2017-03-28 15:01:45 -0400

    sorting out some UI stuff

--- imports/startup/routes.js
+++ imports/startup/routes.js
@@ -1,7 +1,11 @@
+import '/imports/ui/components/header/header.js';
+
+import '/imports/ui/layouts/home/home.js';
 import '/imports/ui/layouts/dashboard/dashboard.js';
 
 Router.route('/', function () {
-  this.render('dashboard');
+  this.render('home');
 });
 
+Router.route('/home/', {name: 'home'});
 Router.route('/dashboard/', {name: 'dashboard'});
